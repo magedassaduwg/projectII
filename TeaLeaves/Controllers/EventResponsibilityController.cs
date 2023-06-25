5bafaa5abe4d0c9e8e77f48167a13ac386a65f57
John Owens <jowens14@my.westga.edu>
2023-06-25 16:58:28 -0400

    Added EventResponsibility Model, DAL, and Controller

---
+++ TeaLeaves/Controllers/EventResponsibilityController.cs
@@ -0,0 +1,20 @@
+﻿using TeaLeaves.DALs;
+
+namespace TeaLeaves.Controllers
+{
+    /// <summary>
+    /// The controller for the EventResponsibilityDAL
+    /// </summary>
+    public class EventResponsibilityController
+    {
+        private EventResponsibilityDAL _eventResponsibilityDAL;
+
+        /// <summary>
+        /// The constructor for the EventResponsibilityController
+        /// </summary>
+        public EventResponsibilityController()
+        {
+            _eventResponsibilityDAL = new EventResponsibilityDAL();
+        }
+    }
+}
---
+++ TeaLeaves/DALs/EventResponsibilityDAL.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+using TeaLeaves.Models;
+
+namespace TeaLeaves.DALs
+{
+    /// <summary>
+    /// The DAL for interacting with the EventResponsibilities table
+    /// </summary>
+    public class EventResponsibilityDAL
+    {
+        /// <summary>
+        /// Inserts a new EventResponsibility row into the database
+        /// </summary>
+        /// <param name="eventResponse"></param>
+        /// <returns></returns>
+        public int AddEventResponsibility(EventResponsibility @eventResponsibility)
+        {
+            string query = @"INSERT INTO EventResponsbilities (EventId, UserId, Name) 
+                             VALUES (@EventId, @UserId, @Name)
+                             select scope_identity()";
+            using (SqlConnection connection = TeaLeavesConnectionstring.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand saveCommand = new SqlCommand(query, connection))
+                {
+                    saveCommand.Parameters.AddWithValue("@EventId", @eventResponsibility.EventId);
+                    saveCommand.Parameters.AddWithValue("@UserId", @eventResponsibility.EventId);
+                    saveCommand.Parameters.AddWithValue("@Name", @eventResponsibility.EventId);
+                    return Convert.ToInt32(saveCommand.ExecuteScalar());
+                }
+            }
+        }
+    }
+}
