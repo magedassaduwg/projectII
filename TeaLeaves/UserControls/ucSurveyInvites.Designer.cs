a40bf1686e3008f3d0f061a5975e7c73c61f9f72
John Owens <jowens14@my.westga.edu>
2023-07-05 12:35:38 -0400

    Added Survey and SurveyOption model classes

--- TeaLeaves/UserControls/ucSurveyInvites.Designer.cs
+++ TeaLeaves/UserControls/ucSurveyInvites.Designer.cs
@@ -31,12 +31,12 @@
             lblAcceptedInvites = new Label();
             lblSurveys = new Label();
             dgvAcceptedInvites = new DataGridView();
-            Category = new DataGridViewTextBoxColumn();
             btnViewSurvey = new Button();
             dgvEventInvites = new DataGridView();
-            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
             btnRefresh = new Button();
             btnViewAnswered = new Button();
+            SurveyName = new DataGridViewTextBoxColumn();
+            SurveyName1 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvAcceptedInvites).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dgvEventInvites).BeginInit();
             SuspendLayout();
@@ -47,7 +47,7 @@
             lblAcceptedInvites.BackColor = SystemColors.Control;
             lblAcceptedInvites.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblAcceptedInvites.ForeColor = Color.DarkOrange;
-            lblAcceptedInvites.Location = new Point(396, 309);
+            lblAcceptedInvites.Location = new Point(328, 309);
             lblAcceptedInvites.Name = "lblAcceptedInvites";
             lblAcceptedInvites.Size = new Size(149, 21);
             lblAcceptedInvites.TabIndex = 28;
@@ -59,7 +59,7 @@
             lblSurveys.BackColor = SystemColors.Control;
             lblSurveys.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             lblSurveys.ForeColor = Color.DarkOrange;
-            lblSurveys.Location = new Point(369, 18);
+            lblSurveys.Location = new Point(314, 20);
             lblSurveys.Name = "lblSurveys";
             lblSurveys.Size = new Size(203, 21);
             lblSurveys.TabIndex = 27;
@@ -71,29 +71,22 @@
             dgvAcceptedInvites.AllowUserToDeleteRows = false;
             dgvAcceptedInvites.BackgroundColor = Color.PapayaWhip;
             dgvAcceptedInvites.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvAcceptedInvites.Columns.AddRange(new DataGridViewColumn[] { Category });
-            dgvAcceptedInvites.Location = new Point(9, 355);
+            dgvAcceptedInvites.Columns.AddRange(new DataGridViewColumn[] { SurveyName1 });
+            dgvAcceptedInvites.Location = new Point(112, 355);
             dgvAcceptedInvites.Name = "dgvAcceptedInvites";
             dgvAcceptedInvites.ReadOnly = true;
             dgvAcceptedInvites.RowTemplate.Height = 25;
             dgvAcceptedInvites.ScrollBars = ScrollBars.Vertical;
             dgvAcceptedInvites.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvAcceptedInvites.Size = new Size(902, 160);
+            dgvAcceptedInvites.Size = new Size(613, 160);
             dgvAcceptedInvites.TabIndex = 26;
-            // 
-            // Category
-            // 
-            Category.DataPropertyName = "Category";
-            Category.HeaderText = "Category";
-            Category.Name = "Category";
-            Category.ReadOnly = true;
             // 
             // btnViewSurvey
             // 
             btnViewSurvey.BackColor = Color.Orange;
             btnViewSurvey.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnViewSurvey.ForeColor = Color.DarkGreen;
-            btnViewSurvey.Location = new Point(970, 65);
+            btnViewSurvey.Location = new Point(764, 65);
             btnViewSurvey.Name = "btnViewSurvey";
             btnViewSurvey.Size = new Size(126, 57);
             btnViewSurvey.TabIndex = 25;
@@ -106,30 +99,22 @@
             dgvEventInvites.AllowUserToDeleteRows = false;
             dgvEventInvites.BackgroundColor = Color.PapayaWhip;
             dgvEventInvites.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvEventInvites.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1 });
-            dgvEventInvites.Location = new Point(9, 65);
+            dgvEventInvites.Columns.AddRange(new DataGridViewColumn[] { SurveyName });
+            dgvEventInvites.Location = new Point(112, 65);
             dgvEventInvites.Name = "dgvEventInvites";
             dgvEventInvites.ReadOnly = true;
             dgvEventInvites.RowTemplate.Height = 25;
             dgvEventInvites.ScrollBars = ScrollBars.Vertical;
             dgvEventInvites.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvEventInvites.Size = new Size(902, 160);
+            dgvEventInvites.Size = new Size(613, 160);
             dgvEventInvites.TabIndex = 21;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            dataGridViewTextBoxColumn1.DataPropertyName = "Category";
-            dataGridViewTextBoxColumn1.HeaderText = "Category";
-            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            dataGridViewTextBoxColumn1.ReadOnly = true;
-            dataGridViewTextBoxColumn1.Width = 240;
             // 
             // btnRefresh
             // 
             btnRefresh.BackColor = Color.Orange;
             btnRefresh.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             btnRefresh.ForeColor = Color.DarkGreen;
-            btnRefresh.Location = new Point(970, 273);
+            btnRefresh.Location = new Point(764, 273);
             btnRefresh.Name = "btnRefresh";
             btnRefresh.Size = new Size(126, 57);
             btnRefresh.TabIndex = 24;
@@ -141,12 +126,28 @@
             btnViewAnswered.BackColor = Color.Orange;
             btnViewAnswered.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
             btnViewAnswered.ForeColor = Color.DarkGreen;
-            btnViewAnswered.Location = new Point(970, 168);
+            btnViewAnswered.Location = new Point(764, 168);
             btnViewAnswered.Name = "btnViewAnswered";
             btnViewAnswered.Size = new Size(126, 57);
             btnViewAnswered.TabIndex = 36;
             btnViewAnswered.Text = "View Answered Survey";
             btnViewAnswered.UseVisualStyleBackColor = false;
+            // 
+            // SurveyName
+            // 
+            SurveyName.DataPropertyName = "Name";
+            SurveyName.HeaderText = "Name";
+            SurveyName.Name = "SurveyName";
+            SurveyName.ReadOnly = true;
+            SurveyName.Width = 240;
+            // 
+            // SurveyName1
+            // 
+            SurveyName1.DataPropertyName = "Name";
+            SurveyName1.HeaderText = "Name";
+            SurveyName1.Name = "SurveyName1";
+            SurveyName1.ReadOnly = true;
+            SurveyName1.Width = 240;
             // 
             // ucSurveyInvites
             // 
@@ -160,7 +161,7 @@
             Controls.Add(btnRefresh);
             Controls.Add(dgvEventInvites);
             Name = "ucSurveyInvites";
-            Size = new Size(1134, 620);
+            Size = new Size(988, 549);
             ((System.ComponentModel.ISupportInitialize)dgvAcceptedInvites).EndInit();
             ((System.ComponentModel.ISupportInitialize)dgvEventInvites).EndInit();
             ResumeLayout(false);
@@ -174,11 +175,11 @@
         private Label lblAcceptedInvites;
         private Label lblSurveys;
         private DataGridView dgvAcceptedInvites;
-        private DataGridViewTextBoxColumn Category;
         private Button btnViewSurvey;
         private DataGridView dgvEventInvites;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private Button btnRefresh;
         private Button btnViewAnswered;
+        private DataGridViewTextBoxColumn SurveyName;
+        private DataGridViewTextBoxColumn SurveyName1;
     }
 }
