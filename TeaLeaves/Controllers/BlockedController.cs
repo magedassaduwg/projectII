0b7ceb2846df29c994f6557e7f06f01178df066c
John Owens <jowens14@my.westga.edu>
2023-07-14 22:53:21 -0400

    Added comments

--- TeaLeaves/Controllers/BlockedController.cs
+++ TeaLeaves/Controllers/BlockedController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TeaLeaves.DALs;
+﻿using TeaLeaves.DALs;
 
 namespace TeaLeaves.Controllers
 {
-    internal class BlockedController
+    /// <summary>
+    /// The controller for the blocked
+    /// </summary>
+    public class BlockedController
     {
         private BlockedDAL _blockedDAL;
 
--- TeaLeaves/DALs/BlockedDAL.cs
+++ TeaLeaves/DALs/BlockedDAL.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using TeaLeaves.Helper;
 
 namespace TeaLeaves.DALs
@@ -11,7 +6,7 @@ namespace TeaLeaves.DALs
     /// <summary>
     /// DAL class for managing database Blocked relationships
     /// </summary>
-    internal class BlockedDAL
+    public class BlockedDAL
     {
 
         /// <summary>
